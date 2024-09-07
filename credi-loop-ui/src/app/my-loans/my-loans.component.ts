5d9a012a716321dfa4b24c3686b522900f179408
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-09-07 15:06:33 +0200

    Update my loans

--- credi-loop-ui/src/app/my-loans/my-loans.component.ts
+++ credi-loop-ui/src/app/my-loans/my-loans.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { ComponentsModule } from '../components/components.module';
+import { Installment } from '../services/dto/lenderboard.dto';
+import { MockDataService } from '../services/mock-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-loans',
   standalone: true,
-  imports: [ComponentsModule],
+  imports: [ComponentsModule, CommonModule],
   templateUrl: './my-loans.component.html',
   styleUrl: './my-loans.component.scss',
 })
-export class MyLoansComponent {}
+export class MyLoansComponent {
+  records?: Installment[];
+  constructor(dataService: MockDataService) {
+    this.records = dataService
+      .getInstallements(12)
+      .sort(
+        (a, b) => a.dueDate.getMilliseconds() - b.dueDate.getMilliseconds()
+      );
+  }
+}
