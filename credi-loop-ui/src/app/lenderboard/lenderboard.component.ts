7a39a59777f60f4f436806762a68a7f1477fb35e
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-09-07 10:11:19 +0200

    Add fuzzy search

--- credi-loop-ui/src/app/lenderboard/lenderboard.component.ts
+++ credi-loop-ui/src/app/lenderboard/lenderboard.component.ts
@@ -1,23 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../services/data.service';
 import { LenderBoardRecord } from '../services/dto/lenderboard.dto';
 import { MockDataService } from '../services/mock-data.service';
 import { CommonModule } from '@angular/common';
+import Fuse from 'fuse.js'; // Import Fuse.js for fuzzy search
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lenderboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './lenderboard.component.html',
-  styleUrl: './lenderboard.component.scss',
+  styleUrls: ['./lenderboard.component.scss'],
 })
 export class LenderboardComponent implements OnInit {
-  boardData?: LenderBoardRecord[];
+  boardData: LenderBoardRecord[] = [];
+  filteredBoardData?: LenderBoardRecord[] = [];
+  searchTerm: string = '';
+  fuse?: Fuse<LenderBoardRecord>; 
 
   constructor(private readonly data: MockDataService) {}
+
   ngOnInit(): void {
     this.data.getLenderBoardRecords().then((response: any) => {
       this.boardData = response;
+      this.filteredBoardData = this.boardData; 
+
+      this.fuse = new Fuse(this.boardData, {
+        keys: [
+          'address',
+          'description',
+        ],
+        threshold: 0.3, 
+      });
     });
+  }
+
+  onSearch(): void {
+    if (!this.searchTerm.trim()) {
+      this.filteredBoardData = this.boardData;
+    } else {
+      const searchResult = this.fuse?.search(this.searchTerm);
+      this.filteredBoardData = searchResult?.map((result) => result.item);
+    }
   }
 }
