94497c1f09ae44313e11025958130f7d71971662
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-09-07 10:03:09 +0200

    Add simple lenderboard data

--- credi-loop-ui/src/app/lenderboard/lenderboard.component.ts
+++ credi-loop-ui/src/app/lenderboard/lenderboard.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { LenderBoardRecord } from '../services/dto/lenderboard.dto';
 import { MockDataService } from '../services/mock-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lenderboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lenderboard.component.html',
   styleUrl: './lenderboard.component.scss',
 })
