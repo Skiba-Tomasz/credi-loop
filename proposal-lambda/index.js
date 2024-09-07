e9407909c7501763f561d65174b75ec7d93173b1
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-09-07 16:25:23 +0200

    ok

--- proposal-lambda/index.js
+++ proposal-lambda/index.js
@@ -46,7 +46,34 @@ export async function handler(event, context) {
     const results = (await docClient.send(proposalsCommand)).Items;
     console.log(results);
     return results;
-  } else {
+  } else if (event.rawPath.endsWith("accept-proposal")) {
+    const body = JSON.parse(event.body);
+    console.log(body.hash);
+
+    const proposalsCommand = new QueryCommand({
+      TableName: "proposals-v1",
+      KeyConditionExpression: "#key = :key AND #hash = :hash",
+      ExpressionAttributeNames: { "#key": "partition", "#hash": "hash" },
+      ExpressionAttributeValues: { ":key": "ALL", ":hash": body.hash },
+    });
+    const results = (await docClient.send(proposalsCommand)).Items;
+    const item = results[0];
+    const command = new PutCommand({
+      TableName: "proposals-v1",
+      Item: {
+        partition: "ALL",
+        hash: item.hash,
+        address: item.address,
+        amount: item.amount,
+        installments: item.installments,
+        apy: item.apy,
+        description: item.description,
+        requestNetworkPayload: item.requestNetworkPayload,
+        accepted: true,
+      },
+    });
+    await docClient.send(command);
+    console.log(results);
   }
 }
 
@@ -65,6 +92,11 @@ function hashCode(str) {
 //   body: `{"address": "testAddress2", "amount": 0.05,"installments": 12,"apy": 5.1,"description": "na komputer","requestNetworkPayload": "yyyy"}`,
 // });
 
-handler({
-  rawPath: "/dev/execute/get-proposals",
-});
+// handler({
+//   rawPath: "/dev/execute/get-proposals",
+// });
+
+// handler({
+//   rawPath: "/dev/execute/accept-proposal",
+//   body: '{"hash": 1437014403}',
+// });
