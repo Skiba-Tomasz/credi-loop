f87fea27645b882fc06a20e1d294cd9d7567bfd5
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-09-08 01:09:56 +0200

    rm

--- credi-loop-ui/src/app/app.routes.ts
+++ credi-loop-ui/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LenderboardComponent } from './lenderboard/lenderboard.component';
 import { HomeComponent } from './home/home.component';
 import { MyLoansComponent } from './my-loans/my-loans.component';
-import { MyBorrowsComponent } from './my-borrows/my-borrows.component';
 import { CrediScoreComponent } from './credi-score/credi-score.component';
 
 export const routes: Routes = [
@@ -21,10 +20,6 @@ export const routes: Routes = [
   {
     path: 'my-loans',
     component: MyLoansComponent,
-  },
-  {
-    path: 'my-borrows',
-    component: MyBorrowsComponent,
   },
   {
     path: '',
