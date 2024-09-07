46ec472049555d92b521be1c6c58ea8fe1b65f68
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2024-09-07 11:52:38 +0200

    save scores

--- calculate-credit-score-lambda/index.js
+++ calculate-credit-score-lambda/index.js
@@ -18,6 +18,9 @@ export async function handler(event, context) {
   console.log("FETCHED ALL");
   console.log(allPayments.length);
   allPayments.forEach((e) => console.log(e));
+  const creditScores = calculateCreditScore(allPayments);
+  creditScores.forEach((value, key) => console.log(`${key}: ${value}`));
+  await saveCreditScores(creditScores);
 }
 
 async function fetchAll() {
@@ -29,9 +32,54 @@ async function fetchAll() {
     if (newPayments.length === 0) {
       return payments;
     }
+    //TODO: just for testing
+    if (payments.length > 0) {
+      return payments;
+    }
     payments = payments.concat(newPayments);
     iterator += newPayments.length;
     console.log(iterator);
+  }
+}
+
+// credit score is number from 0-100
+// for now it has simple algorithm that use transaction count using Request Network to showcase
+// for the future - credit score will assume more parameters like including values of paymens and repaying loans
+function calculateCreditScore(payments) {
+  const startingCreditScore = 50;
+  const maxCreditScore = 100;
+  const bonusForSendingPayment = 0.5;
+  const bonusForReceivingPayment = 1;
+  const creditScores = new Map();
+  for (let i = 0; i < payments.length; i++) {
+    const payment = payments[i];
+    const from = payment.from;
+    const to = payment.to;
+    const fromValue = creditScores.get(from) ?? startingCreditScore;
+    creditScores.set(
+      from,
+      Math.min(fromValue + bonusForReceivingPayment, maxCreditScore)
+    );
+    const toValue = creditScores.get(to) ?? startingCreditScore;
+    creditScores.set(
+      to,
+      Math.min(toValue + bonusForSendingPayment, maxCreditScore)
+    );
+  }
+  return creditScores;
+}
+
+async function saveCreditScores(creditScores) {
+  for (let [key, value] of creditScores) {
+    const command = new PutCommand({
+      TableName: "credit-scores-v1",
+      Item: {
+        partition: "ALL",
+        address: key,
+        creditScore: value,
+      },
+    });
+    await docClient.send(command);
   }
 }
 
@@ -39,12 +87,11 @@ async function fetch(skip, fetchSize) {
   const endpoint =
     "https://gateway.thegraph.com/api/3b77079354a6291da012e3a7c45ecd69/subgraphs/id/4PScFUi3CFDbop9XzT6gCDtD4RR8kRzyrzSjrHoXHZBt";
   const query = {
-    query: `{payments(first: ${fetchSize}, skip: ${skip}) {tokenAddress,amount,amountInCrypto,to,from}}`,
+    query: `{payments(first: ${fetchSize}, skip: ${skip}) {txHash,tokenAddress,amount,amountInCrypto,to,from,currency}}`,
   };
   const data = await axios.post(endpoint, query);
   console.log(data.data.data.payments.length);
   return data.data.data.payments;
-  //   return data?.data?.data?.accounts?.map((result) => result.id);
 }
 
 handler();
