25e27451ead33264915f16576f7f092e13808172
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2024-09-07 21:47:43 +0200

    Deploy this

--- credi-loop-ui/src/app/lenderboard-modal/lenderboard-modal.component.ts
+++ credi-loop-ui/src/app/lenderboard-modal/lenderboard-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { LenderBoardRecordCreate } from '../services/dto/lenderboard.dto';
 import { FormsModule } from '@angular/forms';
+import { MetamaskService } from '../services/metamask.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   standalone: true,
   imports: [FormsModule],
@@ -10,19 +12,37 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./lenderboard-modal.component.scss'],
 })
 export class LenderBoardModalComponent {
+  address?: string;
+
   @Input() record: LenderBoardRecordCreate = {
     address: '',
     amount: 0,
     installments: 0,
     apy: 0,
     description: '',
-    requestNetworkPayload: ''
+    requestNetworkPayload: '',
   };
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    private metamaskService: MetamaskService,
+    private httpClient: HttpClient
+  ) {
+    metamaskService.$userAddress.subscribe(
+      (address) => (this.address = address)
+    );
+  }
 
   onSubmit() {
     console.log(this.record); // Handle form submission
     this.activeModal.close(this.record); // Close modal and pass the form data
+    this.httpClient
+      .post(
+        `https://create-proposal-v1.crediloop.com/create-proposal`,
+        this.record
+      )
+      .subscribe((result) => {
+        console.log(result);
+      });
   }
 }
